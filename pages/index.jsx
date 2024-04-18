a997d5db878a763b74e9ba230f491083cca12d9e
Horizon Development <111465344+testdrake@users.noreply.github.com>
2024-04-17 18:27:58 -0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,8 +55,8 @@ Vote Horizon</a>
           <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
            
               <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
+                <div className="text-white"> 
+                  <p className="text-xl font-semibold mt-5"><i className={`fa-solid fa-music`} /> Optimization</p>
                   <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, MusicMaker gives you a musical pleasure to the fullest.</p>
                 </div>
               </div>
