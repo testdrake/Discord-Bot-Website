2f9b3760bd9c747e1ee7373434cb942f7da32a80
Horizon Development <111465344+testdrake@users.noreply.github.com>
2024-04-17 20:37:08 -0700

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,11 +11,11 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="All-in-One Discord Bot: Setup, Manage, Entertain, and Protect Your Servers!"
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
+          <meta name="twitter:site" content="@Horizon" />
+          <meta name="twitter:creator" content="@Horizon" />
           <meta property="og:url" content="https://horizondev-phi.vercel.app" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Horizon" />
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="All-in-One Discord Bot: Setup, Manage, Entertain, and Protect Your Servers!"
           />
           <meta property="og:image" content="/img/Horizon Logo (1).png" />
           <meta property="og:image:alt" content="Horizon" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Horizon"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/Horizon Logo (1).png" type="image/x-icon" />
