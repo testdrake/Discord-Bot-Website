96868067277e1764ad46dc3aec1bd39deb003b78
Horizon Development <111465344+testdrake@users.noreply.github.com>
2024-12-16 15:52:41 -0800

    Update developers.jsx

--- pages/developers.jsx
+++ pages/developers.jsx
@@ -17,7 +17,7 @@ export default function Developers() {
                 <div className="flex flex-col justify-center text-white rounded">
                     <a href="https://discord.gg/h8dY8SF7RX">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="h-32 w-32 rounded-full" src="https://cdn.discordapp.com/avatars/1044089744230994011/0846ba96944fe37ba48997c58e4dbc44.webp" alt="Owner" />
+                            <img className="h-32 w-32 rounded-full" src="https://bio.horizondevelopment.xyz/pfp2.gif" alt="Owner" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">ĦØŘƗŽØŇ</h1>
                                 <div className="flex items-center mt-1">
